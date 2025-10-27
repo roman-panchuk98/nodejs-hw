7c2043fb221d893d5f5b57af289737d9af91fe4e
Roman Panchuk <panchuk.roman98@gmail.com>
2025-10-27 17:29:10 +0100

    Create first homework Node.js

--- src/server.js
+++ src/server.js
@@ -39,6 +39,7 @@ app.get('/notes', (req, res) => {
 
 app.get('/notes/:noteId', (req, res) => {
   const { noteId } = req.params;
+
   res.status(200).json({
     message: `Retrieved note with ID: ${noteId}`,
   });
